ea973034825d3b58d9e352537e8de525f9a24b36
JackShenYt <61526569+sysytwl@users.noreply.github.com>
2023-10-07 18:12:55 +0100

    Update and rename WS_RC.h to Robot UDP Server.h

--- Robot UDP Server.h
+++ Robot UDP Server.h
@@ -1,17 +1,9 @@
 #include <WiFi.h>
-//#include <ESPmDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
-#include <AsyncTCP.h>
-#include <ESPAsyncWebSrv.h>
-//#include <iostream>
-//#include <sstream>
 #include "ESP32MX1508.h"
-#include "web.h"
 
 
-AsyncWebServer server(80);
-AsyncWebSocket wsCarInput("/CarInput");
 
 MX1508 motorB(25, 26, 0, 1);
 MX1508 motorA(18, 19, 2, 3);
